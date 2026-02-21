eb290c84653be14cafe411f6bce310448ff9626b
Krishna Gupta <krishgupta577@gmail.com>
2026-02-21 14:07:32 +0530

    changes

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260220_094619 from './20260220_094619';
-
-export const migrations = [
-  {
-    up: migration_20260220_094619.up,
-    down: migration_20260220_094619.down,
-    name: '20260220_094619'
-  },
-];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,7 +11,6 @@ import { collections, globals } from './collections'
 import { getServerSideURL } from '@/utilities/getURL'
 import { s3Storage } from '@payloadcms/storage-s3'
 import sharp from 'sharp'
-import { migrations } from './migrations'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -68,7 +67,6 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URL || '',
       options: '-c timezone=Asia/Dubai',
     },
-    prodMigrations: migrations,
   }),
   sharp,
   plugins: [
