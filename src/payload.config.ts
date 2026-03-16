22d575cc5c1efcab7e1d6062552d65939c86cb53
Krishna Gupta <krishgupta577@gmail.com>
2026-03-16 16:11:45 +0530

    changes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,4 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -66,11 +66,8 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URL || '',
-      options: '-c timezone=Asia/Dubai',
-    },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URL || '',
   }),
   sharp,
   plugins: [
