d0c3c379f91e1d6ea200db7150789aa8af35a253
Krishna Gupta <krishgupta577@gmail.com>
2026-03-28 11:21:43 +0530

    chaffes

--- src/collections/Coupon/endpoints/getWebCoupons.ts
+++ src/collections/Coupon/endpoints/getWebCoupons.ts
@@ -1,48 +1,49 @@
 import { PayloadHandler } from 'payload'
 
 export const getShopCouponsHandler: PayloadHandler = async (req) => {
-    const { payload } = req
+  const { payload } = req
 
-    try {
-
-        const coupons = await payload.find({
-            collection: 'coupon',
-            where: {
-                and: [
-                    {
-                        isPubliclyVisible: {
-                            equals: true,
-                        },
-                    },
-                    {
-                        couponFor: {
-                            equals: 'Website',
-                        },
-                    }
-                ],
+  try {
+    const coupons = await payload.find({
+      collection: 'coupon',
+      where: {
+        and: [
+          {
+            isPubliclyVisible: {
+              equals: true,
             },
-            limit: 100,
-            sort: '-createdAt',
-        });
+          },
+          {
+            'couponFor.website': {
+              equals: true,
+            },
+          },
+        ],
+      },
+      limit: 100,
+      sort: '-createdAt',
+    })
 
-        return Response.json({
-            success: true,
-            totalDocs: coupons.totalDocs,
-            docs: coupons.docs.map(coupon => ({
-                id: coupon.id,
-                code: coupon.code,
-                status: coupon.couponStatus,
-                expiryDate: coupon.expiryDate,
-                discountType: coupon.discountType,
-                discountAmount: coupon.discountAmount,
-                minimumAmount: coupon.minimumAmount,
-                applicability: coupon.applicability,
-                products: coupon.products,
-            }))
-        }, { status: 200 })
-
-    } catch (error: any) {
-        console.error('Error fetching shop coupons:', error)
-        return Response.json({ error: 'Internal server error' }, { status: 500 })
-    }
+    return Response.json(
+      {
+        success: true,
+        totalDocs: coupons.totalDocs,
+        docs: coupons.docs.map((coupon) => ({
+          id: coupon.id,
+          code: coupon.code,
+          status: coupon.couponStatus,
+          expiryDate: coupon.expiryDate,
+          discountType: coupon.discountType,
+          discountAmount: coupon.discountAmount,
+          minimumAmount: coupon.minimumAmount,
+          applicability: coupon.applicability,
+          products: coupon.products,
+        })),
+      },
+      { status: 200 },
+    )
+  } catch (error: any) {
+    console.error('Error fetching shop coupons:', error)
+    return Response.json({ error: 'Internal server error' }, { status: 500 })
+  }
 }
