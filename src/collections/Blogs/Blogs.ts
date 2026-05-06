113b4c33ecde1f5ac72cbc28a39e39f677351666
Aaditya7171 <risingaditya91@gmail.com>
2026-05-06 18:40:37 +0530

    No words for this

--- src/collections/Blogs/Blogs.ts
+++ src/collections/Blogs/Blogs.ts
@@ -118,7 +118,7 @@ export const Blogs: CollectionConfig = {
                     },
                   }),
                 ],
-              }),c
+              }),
             },
             {
               name: 'relatedBlogs',
