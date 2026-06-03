1410e49086aeb3a48be0141db6f8a7bb741cbeff
Aaditya7171 <risingaditya91@gmail.com>
2026-06-03 16:35:35 +0530

    Remove unused var

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -31,11 +31,6 @@ export const Media: CollectionConfig = {
         height: 300,
         position: 'centre',
       },
-      {
-        name: 'emailThumb',
-        width: 300,
-        // no height — maintains original aspect ratio, no cropping
-      },
     ],
   },
 }
